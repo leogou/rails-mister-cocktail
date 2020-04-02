6b050d23ca2fc8b8ba4e878ad0d8b08f9d359f62
Leo Gou <leo_g92@hotmail.fr>
2020-04-02 13:04:06 +0200

    Cocktail & Ingredient controllers

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,32 @@
 class IngredientsController < ApplicationController
+  class CocktailsController < ApplicationController
+    before_action :set_ingredient, only: :show
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+
+    if @ingredient.save
+      redirect_to cocktail_path(@ingredient)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name)
+  end
+
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
+  end
+end
 end
